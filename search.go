26d1812e69a8cd3109590a6528b6ec02692ec55f
scottlin <linzhs546@gmail.com>
2023-07-18 00:23:08 +0800

    feat: add support wikipedia search api

---
+++ search.go
@@ -0,0 +1,56 @@
+package wikipedia
+
+import (
+	"context"
+	"errors"
+
+	"github.com/samber/lo"
+)
+
+// SearchOptions are the options for the Wikipedia search request.
+type SearchOptions struct {
+	Limit int // the max number of results returned, default 10
+}
+
+func defaultSearchOptions() *SearchOptions {
+	return &SearchOptions{Limit: 10}
+}
+
+type searchRequest struct {
+	Action   Action `url:"action"`
+	List     string `url:"list"`
+	SrProp   string `url:"srprop"`
+	SrLimit  int    `url:"srlimit"`
+	Limit    int    `url:"limit"`
+	SrSearch string `url:"srsearch"`
+}
+
+// Search searches the Wikipedia for the given query.
+func (c *Client) Search(
+	ctx context.Context,
+	query string,
+	options ...func(searchOptions *SearchOptions),
+) ([]string, error) {
+	if len(query) == 0 {
+		return nil, errors.New("go-wikipedia: query is empty")
+	}
+
+	o := defaultSearchOptions()
+	for _, option := range options {
+		option(o)
+	}
+
+	req := &searchRequest{
+		Action:   ActionQuery,
+		List:     "search",
+		SrLimit:  o.Limit,
+		Limit:    o.Limit,
+		SrSearch: query,
+	}
+	resp, err := c.do(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return lo.Map(resp.Query.Search, func(item *searchResponse, _ int) string { return item.Title }), nil
+}
--- wikipedia.go
+++ wikipedia.go
@@ -1,2 +1,124 @@
 package wikipedia
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/google/go-querystring/query"
+)
+
+// Action is the action of the Wikipedia API request.
+type Action string
+
+const (
+	ActionQuery Action = "query" // query action
+)
+
+const urlWithPlaceholder = "https://%s.wikipedia.org/w/api.php"
+
+// Client is a client for the Wikipedia API requests.
+// Wikipedia API main page: https://www.mediawiki.org/wiki/API:Main_page
+// Wikipedia API docs: https://en.wikipedia.org/api/rest_v1/
+type Client struct {
+	c *http.Client
+	o *options
+
+	url string
+}
+
+// NewClient returns a new instance of the Wikipedia client.
+func NewClient(opts ...Option) (*Client, error) {
+	o := defaultOptions()
+	for _, opt := range opts {
+		opt.apply(o)
+	}
+
+	return &Client{
+		c:   new(http.Client),
+		o:   o,
+		url: fmt.Sprintf(urlWithPlaceholder, o.language),
+	}, nil
+}
+
+type innerQuery struct {
+	Format string `url:"format"`
+	V      any
+}
+
+func newInnerQuery(v any) *innerQuery {
+	return &innerQuery{
+		Format: "json",
+		V:      v,
+	}
+}
+
+type requestError struct {
+	Code string `json:"code"`
+	Info string `json:"info"`
+}
+
+type responseQuery struct {
+	Search []*searchResponse `json:"search"`
+}
+
+type searchResponse struct {
+	Ns        int    `json:"ns"`
+	Title     string `json:"title"`
+	PageID    int    `json:"pageid"`
+	Size      int    `json:"size"`
+	WordCount int    `json:"wordcount"`
+	Snippet   string `json:"snippet"`
+	Timestamp string `json:"timestamp"`
+}
+
+type apiResult struct {
+	Error    requestError   `json:"error"`
+	Query    responseQuery  `json:"query"`
+	Continue map[string]any `json:"continue"`
+	Parse    map[string]any `json:"parse"`
+}
+
+func (c *Client) do(ctx context.Context, v any) (*apiResult, error) {
+	req, err := http.NewRequest(http.MethodGet, c.url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("go-wikipedia: failed to create http request: %w", err)
+	}
+
+	req.WithContext(ctx)
+	req.Header.Set("User-Agent", c.o.userAgent)
+
+	q, err := query.Values(newInnerQuery(v))
+	if err != nil {
+		return nil, fmt.Errorf("go-wikipedia: encode query parameters: %w", err)
+	}
+
+	uq := req.URL.Query()
+	for k, v := range q {
+		for _, vv := range v {
+			uq.Add(k, vv)
+		}
+	}
+
+	req.URL.RawQuery = uq.Encode()
+
+	resp, err := c.c.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("go-wikipedia: http request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("go-wikipedia: read response body: %w", err)
+	}
+
+	res := new(apiResult)
+	if err := json.Unmarshal(body, res); err != nil {
+		return nil, fmt.Errorf("go-wikipedia: unmarshal response body: %w", err)
+	}
+
+	return res, nil
+}
